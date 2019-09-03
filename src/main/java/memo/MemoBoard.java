b9e848e025b11f42993f85171bb133e686aeefe4
maciejkryger <snowyman1979@gmail.com>
2019-09-03 22:28:25 +0200

    refactor, language version

--- src/main/java/memo/MemoBoard.java
+++ src/main/java/memo/MemoBoard.java
@@ -41,27 +41,44 @@ public class MemoBoard {
     }
 
 
-    protected void showWellcomeBoard() {
-        StringBuilder wellcomeBoardBuilder = new StringBuilder();
-        wellcomeBoardBuilder
-                .append("-----------------------------------------------------------------\n")
-                .append("|                           GRA MEMORY                          |\n")
-                .append("-----------------------------------------------------------------\n");
-        System.out.print(wellcomeBoardBuilder);
-
-    }
-
-    protected void printCreatePlayer(int playerNumber) {
-        if (playerNumber == 1) {
-            System.out.print("| podaj imię pierwszego gracza:");
-        } else if (playerNumber == 2) {
-            System.out.print("| podaj imię drugiego gracza:");
+    protected void showWellcomeBoard(int language) {
+        if (language == 1) {
+            StringBuilder wellcomeBoardBuilder = new StringBuilder();
+            wellcomeBoardBuilder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|                           GRA MEMO                            |\n")
+                    .append("-----------------------------------------------------------------\n");
+            System.out.print(wellcomeBoardBuilder);
+        } else {
+            StringBuilder wellcomeBoardBuilder = new StringBuilder();
+            wellcomeBoardBuilder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|                           MEMO GAME                           |\n")
+                    .append("-----------------------------------------------------------------\n");
+            System.out.print(wellcomeBoardBuilder);
         }
     }
 
-    protected void printPlayerCounters(String name1, int counter1, String name2, int counter2) {
-        System.out.printf("|   Gracz1: %s ma odgadniętych kart: %d                   \n", name1, counter1);
-        System.out.printf("|   Gracz2: %s ma odgadniętych kart: %d                   \n", name2, counter2);
+    protected void printCreatePlayer(int playerNumber, int language) {
+        if (playerNumber == 1 && language == 1) {
+            System.out.print("| podaj imie pierwszego gracza:");
+        } else if (playerNumber == 2 && language == 1) {
+            System.out.print("| podaj imie drugiego gracza:");
+        } else if (playerNumber == 1 && language == 2) {
+            System.out.print("| player 1 name:");
+        } else {
+            System.out.print("| player 2 name:");
+        }
+    }
+
+    protected void printPlayerCounters(String name1, int counter1, String name2, int counter2, int language) {
+        if (language == 1) {
+            System.out.printf("|   Gracz1: %s ma punktow: %d                    \n", name1, counter1);
+            System.out.printf("|   Gracz2: %s ma punktow: %d                    \n", name2, counter2);
+        } else {
+            System.out.printf("|   Player1: %s has points: %d                    \n", name1, counter1);
+            System.out.printf("|   Player2: %s has points: %d                    \n", name2, counter2);
+        }
     }
 
 
@@ -92,65 +109,106 @@ public class MemoBoard {
                 v++;
             }
             boardBuilder.append("\n-----------------------------------------------------------------\n");
-
         }
         System.out.print(boardBuilder);
     }
 
 
-    protected void printFieldAreTheSameInfo() {
+    protected void printFieldAreTheSameInfo(int language) {
         StringBuilder builder = new StringBuilder();
-        builder
-                .append("-----------------------------------------------------------------\n")
-                .append("|      >>>  brawo, pola są równe i znikają z puli   <<<         |\n")
-                .append("-----------------------------------------------------------------\n");
+        if (language == 1) {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|      >>>  brawo, pola sa rowne i znikaja z puli   <<<         |\n")
+                    .append("-----------------------------------------------------------------\n");
+        } else {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|    >>>  congrats, fields are the same and disappear   <<<     |\n")
+                    .append("-----------------------------------------------------------------\n");
+        }
         System.out.print(builder);
 
     }
 
-    protected void printFieldNotTheSameInfo() {
+    protected void printFieldNotTheSameInfo(int language) {
         StringBuilder builder = new StringBuilder();
-        builder
-                .append("-----------------------------------------------------------------\n")
-                .append("| >>>  Niestety, pola mają różne wartości, spróbuj ponownie <<< |\n")
-                .append("-----------------------------------------------------------------\n");
+        if (language == 1) {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("| >>>  Niestety, pola maja rozne wartosci, sprobuj ponownie <<< |\n")
+                    .append("-----------------------------------------------------------------\n");
+        } else {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|   >>>    Mistake, fields are not the same, try again   <<<    |\n")
+                    .append("-----------------------------------------------------------------\n");
+        }
         System.out.print(builder);
     }
 
 
-    protected void printWhoMoveIs(String name) {
-        System.out.print("| teraz jest ruch gracza: " + name + "\n");
-    }
-
-    protected void printNumberOfFieldShouldBeDeferent() {
-        System.out.println("| Numery pól muszą być różne!!! ");
-    }
-
-
-    protected void printMakeMove(int fieldNumber) {
-        if (fieldNumber == 1) {
-            System.out.print("| Odkryj pole nr 1:");
-        } else if (fieldNumber == 2) {
-            System.out.print("| Odkryj pole nr 2:");
+    protected void printWhoMoveIs(String name, int language) {
+        if (language == 1) {
+            System.out.print("| teraz jest ruch gracza: " + name + "\n");
+        } else {
+            System.out.print("| now is move of: " + name + "\n");
         }
     }
 
-    protected void printYourMoveShouldBeInteger() {
+    protected void printNumberOfFieldShouldBeDeferent(int language) {
+        if (language == 1) {
+            System.out.println("| Numery pol muszą byc rozne!!! ");
+        } else {
+            System.out.println("| Fields numbers should be different !!! ");
+        }
+    }
+
+
+    protected void printMakeMove(int fieldNumber, int language) {
+        if (fieldNumber == 1 && language ==1) {
+            System.out.print("| Odkryj pole nr 1:");
+        } else if (fieldNumber == 2 && language ==1) {
+            System.out.print("| Odkryj pole nr 2:");
+        }else if (fieldNumber==1 && language ==2){
+            System.out.print("| Please put first field number:");
+        }else {
+            System.out.print("| Please put second field number:");
+        }
+    }
+
+    protected void printYourMoveShouldBeInteger(int language) {
         StringBuilder builder = new StringBuilder();
-        builder
-                .append("-----------------------------------------------------------------\n")
-                .append("|    >>>>  wybór musi być cyfrą, tracisz kolejkę  <<<<          |\n")
-                .append("-----------------------------------------------------------------\n");
+        if(language==1) {
+            builder
+                    .append("\n-----------------------------------------------------------------\n")
+                    .append("|    >>>>  wybor musi być cyfra, tracisz kolejke  <<<<          |\n")
+                    .append("-----------------------------------------------------------------\n");
+        }else {
+            builder
+                    .append("\n-----------------------------------------------------------------\n")
+                    .append("|   >>>>  your choose should be integer, you miss move   <<<<   |\n")
+                    .append("-----------------------------------------------------------------\n");
+        }
         System.out.println(builder);
     }
 
-    protected void printWinner(int counter, String name) {
+    protected void printWinner(int counter, String name, int language) {
         StringBuilder builder = new StringBuilder();
+        if (language==1) {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|             BRAWO !!!  ZWYCIEZCA ZOSTAL GRACZ:                |\n")
+                    .append("|               " + name + " z " + counter + " punktami                  \n")
+                    .append("-----------------------------------------------------------------\n");
+        }else {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|               CONGRATS !!!  THE WINNER IS:                    |\n")
+                    .append("|               " + name + " with " + counter + " points                 \n")
+                    .append("-----------------------------------------------------------------\n");
+        }
         builder
-                .append("-----------------------------------------------------------------\n")
-                .append("|             BRAWO !!!  ZWYCIĘŻCĄ ZOSTAŁ GRACZ:                |\n")
-                .append("|               "+name+" z "+counter+" punktami                  \n")
-                .append("-----------------------------------------------------------------\n")
                 .append("                          **********\n")
                 .append("                           ********\n")
                 .append("                            ******\n")
@@ -161,33 +219,56 @@ public class MemoBoard {
 
     }
 
-    protected void printDraw() {
+    protected void printDraw(int language) {
         StringBuilder builder = new StringBuilder();
-        builder
-                .append("-----------------------------------------------------------------\n")
-                .append("|                    KONIEC GRY ! JEST REMIS !                  |\n")
-                .append("-----------------------------------------------------------------\n");
+        if (language==1) {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|                    KONIEC GRY ! JEST REMIS !                  |\n")
+                    .append("-----------------------------------------------------------------\n");
+        }else {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|                    GAME OVER ! IT's a DRAW !                  |\n")
+                    .append("-----------------------------------------------------------------\n");
+        }
         System.out.print(builder);
     }
 
-    protected void showFinishBoard(int step) {
+    protected void showFinishBoard(int step, int language) {
         StringBuilder builder = new StringBuilder();
-        builder
-                .append("-----------------------------------------------------------------\n")
-                .append("|                gra przebiegła w ").append(step).append(" ruchach :)                 |\n")
-                .append("-----------------------------------------------------------------\n");
+        if (language==1) {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|                gra przebiegla w ").append(step).append(" ruchach :)                 |\n")
+                    .append("-----------------------------------------------------------------\n");
+        }else {
+            builder
+                    .append("-----------------------------------------------------------------\n")
+                    .append("|               the game run in ").append(step).append(" steps :)                    |\n")
+                    .append("-----------------------------------------------------------------\n");
+        }
         System.out.print(builder);
     }
 
-    protected void clearConsole(){
+    protected void clearConsole() {
         StringBuilder builder = new StringBuilder();
-        for (int i=0;i<100; i++){
+        for (int i = 0; i < 100; i++) {
             builder.append("\n");
         }
         System.out.print(builder);
     }
 
-    public void pressEnterToContinue() {
-        System.out.println("by kontynuować naciśnij ENTER");
+    public void pressEnterToContinue(int language) {
+        if(language==1) {
+            System.out.println("by kontynuowac nacisnij ENTER");
+        }else {
+            System.out.println("press ENTER to continue");
+        }
+    }
+
+    public void printLanguageVersionInfo() {
+        System.out.println("| wybierz jezyk/choose language:");
+        System.out.print("| 1-polski/2-english:");
     }
 }
--- src/main/java/memo/MemoController.java
+++ src/main/java/memo/MemoController.java
@@ -18,24 +18,27 @@ public class MemoController {
     private Player player1;
     private String name2;
     private Player player2;
-    Scanner scanner = new Scanner(System.in);
-    int counter = 0;
+    Scanner scanner;
+    private int counter = 0;
+    private int language;
 
 
-    protected MemoController() { }
+    protected MemoController() {
+    }
 
     protected void startGame() {
         board = new MemoBoard(5, 8);
         board.createBoard();
-        createPlayers();
+        language = languageVersionBrowser();
+        createPlayers(language);
         int step = 0;
 
         do {
-            board.showWellcomeBoard();
-            board.printPlayerCounters(player1.getName(), player1.getCounter(), player2.getName(), player2.getCounter());
+            board.showWellcomeBoard(language);
+            board.printPlayerCounters(player1.getName(), player1.getCounter(), player2.getName(), player2.getCounter(), language);
             board.showBoard();
             if (step == 0 || step % 2 == 0) {
-                board.printWhoMoveIs(player1.getName());
+                board.printWhoMoveIs(player1.getName(), language);
                 counter = player1.getCounter();
                 playerMoveGetter();
                 showPlayerFieldFromMove();
@@ -46,7 +49,7 @@ public class MemoController {
 
                 }
             } else {
-                board.printWhoMoveIs(player2.getName());
+                board.printWhoMoveIs(player2.getName(), language);
                 counter = player2.getCounter();
                 playerMoveGetter();
                 showPlayerFieldFromMove();
@@ -57,19 +60,31 @@ public class MemoController {
                 }
             }
             step++;
+            waitForEnter();
             board.clearConsole();
         } while (finishGame());
         whoIsWinner();
-        board.showFinishBoard(step);
+        board.showFinishBoard(step, language);
     }
 
-    private void createPlayers() {
-        board.showWellcomeBoard();
-        board.printCreatePlayer(1);
+    private int languageVersionBrowser() {
+        scanner=new Scanner(System.in);
+        int language = 0;
+        do {
+            board.printLanguageVersionInfo();
+            language = scanner.nextInt();
+        } while (language == 0);
+        return language;
+    }
+
+    private void createPlayers(int language) {
+        scanner=new Scanner(System.in);
+        board.showWellcomeBoard(language);
+        board.printCreatePlayer(1, language);
         name1 = scanner.nextLine();
         player1 = new Player(name1);
         player1.setCounter(0);
-        board.printCreatePlayer(2);
+        board.printCreatePlayer(2, language);
         name2 = scanner.nextLine();
         player2 = new Player(name2);
         player2.setCounter(0);
@@ -87,13 +102,11 @@ public class MemoController {
         }
         boardMapTemp = createTempBoardMap();
         board.showTempBoard(boardMapTemp);
-        waitForEnter();
-        board.clearConsole();
     }
 
     private boolean moveChecker() {
         if (showSign1 == showSign2) {
-            board.printFieldAreTheSameInfo();
+            board.printFieldAreTheSameInfo(language);
             for (Integer i = 0; i < board.boardMap.size(); i++) {
                 String sign = board.boardMap.get(i);
                 if (sign == showSign1) {
@@ -103,25 +116,24 @@ public class MemoController {
             }
             return true;
         } else {
-            board.printFieldNotTheSameInfo();
-            scanner.nextLine();
+            board.printFieldNotTheSameInfo(language);
             return false;
         }
     }
 
     private void playerMoveGetter() {
         try {
-            board.printMakeMove(1);
+            board.printMakeMove(1, language);
             move1 = scanner.nextInt();
             do {
-                board.printMakeMove(2);
+                board.printMakeMove(2, language);
                 move2 = scanner.nextInt();
                 if (move1 == move2) {
-                    board.printNumberOfFieldShouldBeDeferent();
+                    board.printNumberOfFieldShouldBeDeferent(language);
                 }
             } while (move1 == move2);
-        }catch (InputMismatchException e){
-            board.printYourMoveShouldBeInteger();
+        } catch (InputMismatchException e) {
+            board.printYourMoveShouldBeInteger(language);
         }
     }
 
@@ -150,17 +162,17 @@ public class MemoController {
     }
 
     private void whoIsWinner() {
-        if (player1.getCounter() > player2.getCounter()){
-            board.printWinner(player1.getCounter(),player1.getName());
-        } else if(player2.getCounter() > player1.getCounter()) {
-            board.printWinner(player2.getCounter(),player2.getName());
+        if (player1.getCounter() > player2.getCounter()) {
+            board.printWinner(player1.getCounter(), player1.getName(), language);
+        } else if (player2.getCounter() > player1.getCounter()) {
+            board.printWinner(player2.getCounter(), player2.getName(),language);
         } else {
-            board.printDraw();
+            board.printDraw(language);
         }
     }
 
-    private void waitForEnter(){
-        board.pressEnterToContinue();
+    private void waitForEnter() {
+        board.pressEnterToContinue(language);
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
     }
